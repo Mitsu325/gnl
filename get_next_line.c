c087512412991e30fc480829900b707fd1b27f7d
pmitsuko <pmitsuko@student.42sp.org.br>
2021-02-25 09:58:27 -0300

    create function strjoin_free

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,44 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 08:27:16 by pmitsuko          #+#    #+#             */
-/*   Updated: 2021/02/24 20:45:12 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2021/02/25 09:57:59 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+int	strjoin_free(char *s1, char *s2)
+{
+	int		s1_len;
+	int		s2_len;
+	char	*new_str;
+
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(new_str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char))))
+		return (NULL);
+	ft_memcpy()
+}
+
 int	get_next_line(int fd, char **line)
 {
-	int			ret;
+	static char	*str = NULL;
 	char		buff[BUFFER_SIZE + 1];
-	int			l = 0;
-	char		*break_line;
-//	static char	*str;
+	int			ret;
+	int			index_bl;
 
 	if (fd < 0 || line == NULL)
-	{
-		printf("ERROR");
-		return (-1);
-	}
+		return (R_ERROR);
 	while ((ret = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		*(buff + ret) = '\0';
-//		printf("%d: %s\n", l++, buff);
+		str = strjoin_free(str, buff);
+		printf("%s \n\n", str);
 	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,23 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 22:05:48 by pmitsuko          #+#    #+#             */
-/*   Updated: 2021/02/24 09:40:56 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2021/02/25 09:49:13 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 8
 #endif
 
-#include <stdlib.h>
-#include <unistd.h>
+# define R_LINE 1
+# define R_EOF 0
+# define R_ERROR -1
 
 int		get_next_line(int fd, char **line);
 
